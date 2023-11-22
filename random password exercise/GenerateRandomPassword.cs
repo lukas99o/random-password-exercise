5887fe077e1b22f177417f44a69f10d61dddcb29
lukas99o <lukas99o@hotmail.com>
2023-11-22 15:23:14 +0100

    Add project files.

---
+++ random password exercise/GenerateRandomPassword.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace random_password_exercise
+{
+    internal static class GenerateRandomPassword
+    {
+        private static int _passwordLength;
+        private static string _numbers = "0123456789";
+        private static string _alphabet = "abcdefghijklmnopqrstuvwxyz"
+        private static string _specials = "!?#%&@";
+        private static string _password;
+        public static int PasswordLength { get; set; }
+        public static bool ContainsNumbers { get; set; }
+        public static bool SpecialCharacters { get; set; }
+        public static string Password { get; set; }
+
+        public static string GenerateRandomString(int passwordLength, )
+        {
+            _password += _alphabet;
+
+            if (ContainsNumbers)
+            {
+                _password += _numbers;
+            }
+
+            if (SpecialCharacters)
+            {
+                _password += _specials;
+            }
+
+            if (ContainsNumbers && SpecialCharacters)
+            {
+                _password += _specials;
+                _password += _numbers;
+            }
+
+            var passwordLength = new Random();
+            passwordLength.Next(0, PasswordLength);
+
+            return _password;
+        }
+    }
+}
---
+++ random password exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace random_password_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a password length: ");
+            string passwordLength = Console.ReadLine();
+            Console.Write("Will the password contain numbers?");
+            string containNumbers = Console.ReadLine();
+            Console.WriteLine("Will the password contain special characters");
+            string specialCharacters = Console.ReadLine();
+
+            
+        }
+    }
+}
